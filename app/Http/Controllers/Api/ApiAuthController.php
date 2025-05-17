732b928a7c68b194aeb5f18a23153947727d0e08
andiks2018 <andiks2018@gmail.com>
2025-05-17 07:25:00 +0700

    loginnya dirubah codenya biar lebih rapi dengan menambahkan make LoginRequest.php

--- app/Http/Controllers/Api/ApiAuthController.php
+++ app/Http/Controllers/Api/ApiAuthController.php
@@ -3,37 +3,34 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\LoginRequest;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 
 class ApiAuthController extends Controller
 {
     //selanjutnya kita buat fungsi
-    public function login (Request $request)
+    public function login (LoginRequest $request)
     {
-        // validate request
-        $request->validate([
-            'email' => 'required|email',
-            'password' => 'required'
-        ]);
-        // check if user is exist
-        $user = User::where('email', $request->email)->first();
-        // check if password is correct
-        if (!$user || !Hash::check($request->password, $user->password)) {
+        // validate dengan auth::attempt
+        // $credentials = $request->only('email', 'password');
+        if (Auth::attempt($request->only('email', 'password'))) {
+            // jika berhasil login, kita ambil usernya
+            $user = User::where('email', $request->email)->first();
+            $token = $user->createToken('token')->plainTextToken;
+            return response()->json([
+                'message' => 'Login success',
+                'token' => $token,
+                'user' => $user,
+            ]);
+        } else {
+            // jika gagal login
             return response()->json([
                 'message' => 'coba cek email dan password anda'
             ], 401);
         }
-        // generate token
-        $token = $user->createToken('token')->plainTextToken;
-        // return $user->createToken('token')->plainTextToken;
-        // return response ini alternatif dengan
-        return response()->json([
-            'message' => 'Login success',
-            'token' => $token,
-            'user' => $user,
-        ]);
     }
 
     public function register (Request $request)
